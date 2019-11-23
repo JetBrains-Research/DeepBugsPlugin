6d001461663c03db91e50862890f898eff2852f1
Vladislav Tankov <vdtankov@gmail.com>
2019-11-24 01:34:49 +0300

    Remove few unused

--- common/src/main/kotlin/org/jetbrains/research/deepbugs/common/utils/Cbor.kt
+++ common/src/main/kotlin/org/jetbrains/research/deepbugs/common/utils/Cbor.kt
@@ -6,6 +6,7 @@ import kotlinx.serialization.cbor.Cbor
 object Cbor {
     val cbor = Cbor()
 
+    @Suppress("unused")
     inline fun <reified T> bytes(value: T, serializer: KSerializer<T>) = cbor.dump(serializer, value)
     inline fun <reified T> parse(value: ByteArray, serializer: KSerializer<T>) = cbor.load(serializer, value)
 }
