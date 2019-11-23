3edb3691119d6bbe9cec6cbe6911464cd5e6fe80
Anastasia Tuchina <anastasia.i.tuchina@gmail.com>
2019-11-24 01:52:47 +0300

    Log error report

--- common/src/main/kotlin/org/jetbrains/research/deepbugs/common/model/ModelManager.kt
+++ common/src/main/kotlin/org/jetbrains/research/deepbugs/common/model/ModelManager.kt
@@ -4,11 +4,13 @@ import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 import org.jetbrains.research.deepbugs.common.DeepBugsPlugin
+import org.jetbrains.research.deepbugs.common.ide.fus.collectors.counter.ErrorInfoCollector
 import org.jetbrains.research.deepbugs.common.utils.Cbor
 import org.jetbrains.research.deepbugs.common.utils.Mapping
 import org.tensorflow.SavedModelBundle
 import org.tensorflow.Session
 import java.io.File
+import java.lang.Exception
 
 object ModelManager : StartupActivity, DumbAware {
     private val modelFolder by lazy { File(DeepBugsPlugin.installationFolder, "models") }
@@ -18,9 +20,9 @@ object ModelManager : StartupActivity, DumbAware {
 
     override fun runActivity(project: Project) {
         storage = ModelStorage(
-            binOperandModel = loadModel("binOperandDetectionModel"),
-            binOperatorModel = loadModel("binOperatorDetectionModel"),
-            swappedArgsModel = loadModel("swappedArgsDetectionModel"),
+            binOperandModel = loadModel("binOperandDetectionModel")!!,
+            binOperatorModel = loadModel("binOperatorDetectionModel")!!,
+            swappedArgsModel = loadModel("swappedArgsDetectionModel")!!,
             nodeTypeMapping = loadMapping("nodeTypeToVector.cbor"),
             typeMapping = loadMapping("typeToVector.cbor"),
             operatorMapping = loadMapping("operatorToVector.cbor"),
@@ -30,5 +32,10 @@ object ModelManager : StartupActivity, DumbAware {
 
     private fun loadMapping(name: String): Mapping = Cbor.parse(File(modelFolder, name).readBytes(), Mapping.serializer())
 
-    private fun loadModel(name: String): Session = SavedModelBundle.load(File(modelFolder, name).canonicalPath, "serve").session()
+    private fun loadModel(name: String): Session? = try {
+        SavedModelBundle.load(File(modelFolder, name).canonicalPath, "serve").session()
+    } catch (ex: Exception) {
+        ErrorInfoCollector.logInitErrorReported()
+        null
+    }
 }
--- common/src/main/kotlin/org/jetbrains/research/deepbugs/common/utils/platform/PlatformManager.kt
+++ common/src/main/kotlin/org/jetbrains/research/deepbugs/common/utils/platform/PlatformManager.kt
@@ -42,5 +42,3 @@ object PlatformManager {
         }
     }
 }
-
-
