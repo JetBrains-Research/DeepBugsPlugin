3a62ede2aad93715572662d4a782aef35bcbd887
Vladislav.Tankov <vdtankov@gmail.com>
2019-11-27 22:11:25 +0300

    Add italics to tooltips. Change Icon of ignore quickfix

--- js-plugin/src/main/kotlin/org/jetbrains/research/deepbugs/javascript/ide/quickfixes/JSIgnoreExpressionQuickFix.kt
+++ js-plugin/src/main/kotlin/org/jetbrains/research/deepbugs/javascript/ide/quickfixes/JSIgnoreExpressionQuickFix.kt
@@ -2,13 +2,16 @@ package org.jetbrains.research.deepbugs.javascript.ide.quickfixes
 
 import com.intellij.codeInspection.LocalQuickFix
 import com.intellij.codeInspection.ProblemDescriptor
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.command.undo.BasicUndoableAction
 import com.intellij.openapi.command.undo.UndoManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Iconable
 import org.jetbrains.research.deepbugs.javascript.JSDeepBugsConfig
 import org.jetbrains.research.deepbugs.javascript.JSResourceBundle
+import javax.swing.Icon
 
-class JSIgnoreExpressionQuickFix(private val expr: String) : LocalQuickFix {
+class JSIgnoreExpressionQuickFix(private val expr: String) : LocalQuickFix, Iconable {
     override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
         val ignore = object : BasicUndoableAction(descriptor.psiElement?.containingFile?.virtualFile) {
             override fun redo() = JSDeepBugsConfig.ignoreExpression(expr)
@@ -17,6 +20,8 @@ class JSIgnoreExpressionQuickFix(private val expr: String) : LocalQuickFix {
         ignore.redo()
         UndoManager.getInstance(project).undoableActionPerformed(ignore)
     }
+
+    override fun getIcon(flags: Int): Icon = AllIcons.Actions.Cancel
 
     override fun getFamilyName(): String = JSResourceBundle.message("deepbugs.javascript.display")
     override fun getName(): String = JSResourceBundle.message("deepbugs.javascript.ignore.quickfix", expr)
--- py-plugin/src/main/kotlin/org/jetbrains/research/deepbugs/python/ide/quickfixes/PyIgnoreExpressionQuickFix.kt
+++ py-plugin/src/main/kotlin/org/jetbrains/research/deepbugs/python/ide/quickfixes/PyIgnoreExpressionQuickFix.kt
@@ -2,13 +2,16 @@ package org.jetbrains.research.deepbugs.python.ide.quickfixes
 
 import com.intellij.codeInspection.LocalQuickFix
 import com.intellij.codeInspection.ProblemDescriptor
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.command.undo.BasicUndoableAction
 import com.intellij.openapi.command.undo.UndoManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Iconable
 import org.jetbrains.research.deepbugs.python.PyDeepBugsConfig
 import org.jetbrains.research.deepbugs.python.PyResourceBundle
+import javax.swing.Icon
 
-class PyIgnoreExpressionQuickFix(private val expr: String) : LocalQuickFix {
+class PyIgnoreExpressionQuickFix(private val expr: String) : LocalQuickFix, Iconable {
     override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
         val ignore = object : BasicUndoableAction(descriptor.psiElement?.containingFile?.virtualFile) {
             override fun redo() = PyDeepBugsConfig.ignoreExpression(expr)
@@ -18,11 +21,8 @@ class PyIgnoreExpressionQuickFix(private val expr: String) : LocalQuickFix {
         UndoManager.getInstance(project).undoableActionPerformed(ignore)
     }
 
-    override fun getFamilyName(): String {
-        return PyResourceBundle.message("deepbugs.python.display")
-    }
+    override fun getIcon(flags: Int): Icon = AllIcons.Actions.Cancel
 
-    override fun getName(): String {
-        return PyResourceBundle.message("deepbugs.python.ignore.quickfix", expr)
-    }
+    override fun getFamilyName(): String = PyResourceBundle.message("deepbugs.python.display")
+    override fun getName(): String = PyResourceBundle.message("deepbugs.python.ignore.quickfix", expr)
 }
