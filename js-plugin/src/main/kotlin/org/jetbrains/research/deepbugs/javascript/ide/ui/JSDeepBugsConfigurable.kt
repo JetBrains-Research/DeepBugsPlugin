26a65e4688a6c0e308897686c7f71c4d257c8d1f
Anastasia Tuchina <anastasia.i.tuchina@gmail.com>
2019-11-28 17:11:34 +0300

    Add comment to customization reset button

--- js-plugin/src/main/kotlin/org/jetbrains/research/deepbugs/javascript/ide/ui/JSDeepBugsConfigurable.kt
+++ js-plugin/src/main/kotlin/org/jetbrains/research/deepbugs/javascript/ide/ui/JSDeepBugsConfigurable.kt
@@ -3,12 +3,13 @@ package org.jetbrains.research.deepbugs.javascript.ide.ui
 import com.intellij.openapi.components.ServiceManager
 import org.jetbrains.research.deepbugs.common.DeepBugsConfig
 import org.jetbrains.research.deepbugs.common.ide.ui.DeepBugsConfigurable
-import org.jetbrains.research.deepbugs.common.ide.ui.DeepBugsSettingsPanel
 import org.jetbrains.research.deepbugs.javascript.JSDeepBugsConfig
 import org.jetbrains.research.deepbugs.javascript.JSResourceBundle
 
-class JSDeepBugsConfigurable : DeepBugsConfigurable(JSDeepBugsConfig.default, "deepbugs.js.configurable", JSResourceBundle.message("deepbugs.javascript.display")) {
-    override fun createUi(): DeepBugsSettingsPanel = DeepBugsSettingsPanel(settings, default)
-
+class JSDeepBugsConfigurable : DeepBugsConfigurable(
+    JSDeepBugsConfig.default,
+    "deepbugs.js.configurable",
+    JSResourceBundle.message("deepbugs.javascript.display")
+) {
     override fun getSettings(): DeepBugsConfig = ServiceManager.getService(JSDeepBugsConfig::class.java)
 }
