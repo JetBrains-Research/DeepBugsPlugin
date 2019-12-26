185b3bf2c0678db76f89e87b6b6d77d953b8d1d6
Anastasia Tuchina <anastasia.i.tuchina@gmail.com>
2019-12-26 14:53:34 +0300

    Disable logger

--- common/src/main/kotlin/org/jetbrains/research/deepbugs/common/ide/fus/DeepBugsEventLoggerProvider.kt
+++ common/src/main/kotlin/org/jetbrains/research/deepbugs/common/ide/fus/DeepBugsEventLoggerProvider.kt
@@ -6,11 +6,11 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.util.registry.Registry
 
 class DeepBugsEventLoggerProvider : StatisticsEventLoggerProvider("DBP", 1) {
-    override fun isRecordEnabled(): Boolean =
-        !ApplicationManager.getApplication().isUnitTestMode &&
-            Registry.`is`("feature.usage.event.log.collect.and.upload") &&
-            StatisticsUploadAssistant.isCollectAllowed()
+    override fun isRecordEnabled(): Boolean = false
+        //!ApplicationManager.getApplication().isUnitTestMode &&
+        //    Registry.`is`("feature.usage.event.log.collect.and.upload") &&
+        //    StatisticsUploadAssistant.isCollectAllowed()
 
-    override fun isSendEnabled(): Boolean =
-        isRecordEnabled() && StatisticsUploadAssistant.isSendAllowed()
+    override fun isSendEnabled(): Boolean = false
+        //isRecordEnabled() && StatisticsUploadAssistant.isSendAllowed()
 }
