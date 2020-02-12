bdbeced3e09526f1e607e25fcd7a12f37c90771c
Anastasia Tuchina <anastasia.i.tuchina@gmail.com>
2020-02-12 17:03:16 +0300

    Add access modifiers

--- common/src/main/kotlin/org/jetbrains/research/deepbugs/common/model/AbstractStorage.kt
+++ common/src/main/kotlin/org/jetbrains/research/deepbugs/common/model/AbstractStorage.kt
@@ -3,8 +3,8 @@ package org.jetbrains.research.deepbugs.common.model
 abstract class AbstractStorage<T> {
     protected open val storage: Map<String, T> = emptyMap()
 
-    abstract val ext: String
-    abstract fun load(name: String): T?
+    protected abstract val ext: String
+    protected abstract fun load(name: String): T?
 
     operator fun get(name: String): T? = storage[name] ?: load("$name.$ext")
 }
